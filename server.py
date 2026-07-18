25349614113c2187d60c5f30404cad262a97c549
stablestreamsllc <stable.streamsllc@gmail.com>
2026-07-18 17:18:55 +0100

    Change default host to '0.0.0.0' in server.py

--- server.py
+++ server.py
@@ -1588,6 +1588,6 @@ if __name__ == "__main__":
     import uvicorn
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=8080)
-    parser.add_argument("--host", type=str, default="127.0.0.1")
+    parser.add_argument("--host", type=str, default="0.0.0.0")
     args = parser.parse_args()
     uvicorn.run(app, host=args.host, port=args.port)
